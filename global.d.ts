59d00e8aa44f3d9ef7ea232913ba1eee99f005d6
k-logic24 <49090551+k-logic24@users.noreply.github.com>
2022-05-11 07:11:11 +0900

    環境変数 型追加

--- global.d.ts
+++ global.d.ts
@@ -4,5 +4,6 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_MICROCMS_API_KEY: string
     readonly NEXT_PUBLIC_MICROCMS_BASE_URL: string
     readonly NEXT_PUBLIC_BASE_URL: string
+    readonly NEXT_PUBLIC_GA_MEASUREMENT_ID: string
   }
 }
