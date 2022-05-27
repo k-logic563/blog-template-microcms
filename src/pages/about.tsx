f76eb2026bb338afdb815e66b844b57ec36e445b
k-logic24 <49090551+k-logic24@users.noreply.github.com>
2022-05-27 15:08:07 +0900

    プロフィールページ修正

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import type { NextPageWithLayout } from 'next'
 import { NextSeo } from 'next-seo'
-import { Box, Heading, Image, Table, Tbody, Tr, Td } from '@chakra-ui/react'
+import { Box, Heading, Image, Text, HStack } from '@chakra-ui/react'
 
 import Layout from '@/layout'
-
-import { profiles } from '@/constants/pages'
 
 const AboutPage: NextPageWithLayout = () => {
   return (
@@ -20,24 +18,29 @@ const AboutPage: NextPageWithLayout = () => {
       />
       <Box>
         <Heading as="h1" fontSize={{ base: '24px', md: '28px' }} mb={4}>
-          プロフィール
+          このサイトについて
         </Heading>
-        <Image mb={4} rounded="5px" src="/assets/images/iwt.jpg" alt="iwt" />
-        <Table variant="simple">
-          <Tbody>
-            {profiles.map((x, idx) => (
-              <Tr key={idx}>
-                <Td fontWeight="bold" whiteSpace="nowrap">
-                  {x.title}
-                </Td>
-                <Td
-                  lineHeight="1.8"
-                  dangerouslySetInnerHTML={{ __html: x.description }}
-                />
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
+        <Box lineHeight={1.8}>
+          <Text mb={4}>
+            このサイトは日頃の業務から個人開発まで、気づきや勉強になったことをまとめるテックブログサイトになります。
+            <br />
+            「あれ？前も同じ問題に出会った気がするなぁ。」をなるべく減らすために、自分がつまづいた部分も積極的に記事にまとめていきます。
+          </Text>
+          <Text mb={8}>
+            なるべく思った時に更新するようにしていますが、怠け癖があるため、更新頻度にばらつきがあります。人間だもの。
+          </Text>
+          <HStack columnGap={2}>
+            <Image
+              w="80px"
+              h="80px"
+              rounded="full"
+              objectFit="cover"
+              src="/assets/images/iwt.jpg"
+              alt="iwt"
+            />
+            <Text>いわを</Text>
+          </HStack>
+        </Box>
       </Box>
     </>
   )
