7c5c35272fd28d1854a9a4f25497dbdacd35756b
k-logic24 <k.log.ic563@gmail.com>
2022-08-02 23:27:28 +0900

    umami追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="js">
-        <Head />
+        <Head>
+          <script async defer data-website-id="7e2007d1-5182-470a-99cb-75ce41ee0dd5" src="https://iwtttter-umami.vercel.app/umami.js"></script>
+        </Head>
         <body>
           <ColorModeScript initialColorMode={config.initialColorMode} />
           <Main />
