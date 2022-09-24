35781381ee73c5beb4f7f425816772b5ae8b7e68
k-logic24 <k.log.ic563@gmail.com>
2022-09-24 23:03:26 +0900

    リファクタリング

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,20 +11,6 @@ export default class Document extends NextDocument {
             data-website-id="7e2007d1-5182-470a-99cb-75ce41ee0dd5"
             src="https://iwtttter-umami.vercel.app/umami.js"
           ></script>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
