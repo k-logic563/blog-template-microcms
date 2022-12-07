e849424190383245290b29f1da947c61ee8195be
k-logic24 <k.log.ic563@gmail.com>
2022-12-07 22:57:03 +0900

    スタイル調整

--- src/feature/home/PopularList.tsx
+++ src/feature/home/PopularList.tsx
@@ -26,28 +26,23 @@ export const PopularList = <T extends ContentsProps>({
     <div className="flex flex-wrap justify-center gap-[24px] lg:gap-x-[32px] lg:gap-y-[32px]">
       {contents.map((item, index) => (
         <Link key={`${item.id}_${index}`} href={`/blog/${item.id}`} passHref>
-          <a className="relative w-full sm:w-[calc(50%-1/2*24px)] lg:w-[calc(33.3333%-2/3*32px)] transition-all duration-300 hover:shadow-[0_0_30px_rgba(49,49,49,0.3)]">
+          <a className="relative w-full sm:w-[calc(50%-1/2*24px)] lg:w-[calc(33.3333%-2/3*32px)] transition-all duration-150 hover:opacity-60">
             <div
-              className={`grid place-items-center text-xl font-bold font-roboto italic w-[45px] h-[45px] bg-white rounded-full absolute left-0 top-0 -translate-y-1/3 -translate-x-1/3 text-white ${rankTextClass[index]}`}
+              className={`grid place-items-center text-xl font-bold font-roboto italic w-[45px] h-[45px] bg-white rounded-full absolute left-0 top-0 -translate-y-1/4 -translate-x-1/4 text-white ${rankTextClass[index]}`}
             >
               {index + 1}
             </div>
-            <Image
-              className="w-full h-[160px] md:h-[180px] object-cover rounded-tr-lg"
-              src={item.eyecatch.url}
-              alt=""
-            />
-            <div className="p-4 bg-white rounded-b-lg">
-              <div className="mb-3">
-                <span className="text-[12px] font-bold inline-block bg-teal-500 rounded-full text-white px-[0.8em] py-[0.3em]">
-                  {item.category.name}
-                </span>
-              </div>
-              <p className="md:text-[18px] font-bold mb-3">{item.title}</p>
-              <p className="mt-auto text-right text-[14px] text-gray-400 font-roboto font-medium">
-                {formatDate(item.publishedAt)}
-              </p>
-            </div>
+            <figure className="mb-4">
+              <Image
+                className="w-full h-[160px] md:h-[180px] object-cover rounded-lg"
+                src={item.eyecatch.url}
+                alt=""
+              />
+            </figure>
+            <p className="text-[14px] text-gray-400 font-roboto font-medium mb-[0.1em]">
+              {formatDate(item.publishedAt)}
+            </p>
+            <p className="md:text-[18px] font-bold mb-3 grow">{item.title}</p>
           </a>
         </Link>
       ))}
