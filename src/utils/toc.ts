44f1961e5b2edc11a70903fe549da4455c77891a
k-logic24 <k.log.ic563@gmail.com>
2022-12-29 09:47:59 +0900

    目次は見出し3つ以上で生成するようにする

--- src/utils/toc.ts
+++ src/utils/toc.ts
@@ -2,6 +2,9 @@ import { CheerioAPI } from 'cheerio'
 
 export const generateToc = ($: CheerioAPI) => {
   const headings = $('h2, h3, h4').toArray()
+
+  if (headings.length < 3) return []
+
   return headings.map((x) => ({
     text: (x.children[0] as any).data,
     id: x.attribs.id,
