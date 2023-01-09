d6115cdc201760937a0f047c5d569b05ad704dcd
k-logic24 <k.log.ic563@gmail.com>
2023-01-09 10:18:39 +0900

    エラーページリファクタリング

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -5,18 +5,23 @@ import { NextSeo } from 'next-seo'
 import { MainLayout } from '@/components/Layout'
 
 interface Props {
-  title: string
-  description: string
+  statusCode: number | undefined
 }
 
-const ErrorPage: NextPageWithLayout<Props> = ({ title, description }) => {
+const ErrorPage: NextPageWithLayout<Props> = ({ statusCode }) => {
   return (
     <>
-      <NextSeo title={title} noindex />
-      <h1 className="mb-[0.6em] text-center text-[24px] font-bold md:text-[32px]">
-        {title}
-      </h1>
-      <p className="mb-[2.4em] text-center">{description}</p>
+      <NextSeo title="エラーページ" noindex />
+      {statusCode && (
+        <h1 className="mb-[0.6em] text-center text-[24px] font-bold md:text-[32px]">
+          {statusCode}
+        </h1>
+      )}
+      <p className="mb-[2.4em] text-center">
+        {statusCode
+          ? `${statusCode}エラーが発生しました`
+          : 'アプリケーション側のエラーが発生しました'}
+      </p>
       <div className="text-center">
         <Link
           className="inline-block rounded bg-teal-500 py-[.5em] px-[1em] font-bold text-white transition-all duration-150 hover:bg-teal-600"
@@ -30,22 +35,11 @@ const ErrorPage: NextPageWithLayout<Props> = ({ title, description }) => {
 }
 
 ErrorPage.getInitialProps = ({ res, err }: NextPageContext) => {
-  const props = {
-    title: 'Unknown Error',
-    description: '不明なエラーが発生しました。',
-  }
-  const statusCode = res ? res.statusCode : err ? err.statusCode ?? 500 : 404
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
 
-  if (statusCode === 404) {
-    props.title = '404 Not Found'
-    props.description = '入力したページが発見できませんでした。'
+  return {
+    statusCode,
   }
-  if (statusCode === 500) {
-    props.title = '500 Internal Server Error'
-    props.description = 'サーバーエラーによりページが表示できませんでした。'
-  }
-
-  return props
 }
 
 export default ErrorPage
