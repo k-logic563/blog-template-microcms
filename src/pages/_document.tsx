0be6a097f5d8573b5b0c3c80834e999228cf9b62
k-logic24 <k.log.ic563@gmail.com>
2023-01-28 21:05:19 +0900

    umamiを除く

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,14 +9,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="js">
-        <Head>
-          <script
-            async
-            defer
-            data-website-id="7e2007d1-5182-470a-99cb-75ce41ee0dd5"
-            src="https://iwtttter-umami.vercel.app/umami.js"
-          ></script>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
